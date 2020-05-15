d8521e9b4de1b726592c588dc35127764368bc59
Paulo Ricardo F. S. Junior <pauloricardo.sistemas@hotmail.com>
2020-05-14 23:41:33 -0300

    initial commit

---
+++ src/routes.js
@@ -0,0 +1,28 @@
+import 'react-native-gesture-handler';
+import { createAppContainer } from 'react-navigation';
+
+import { createStackNavigator } from 'react-navigation-stack';
+
+import Main from './pages/Main';
+import User from './pages/User';
+
+const Routes = createAppContainer(
+    createStackNavigator(
+        {
+            Main,
+            User,
+        },
+        {
+            defaultNavigationOptions: {
+                headerTitleAlign: 'center',
+                headerBackTitleVisible: false,
+                headerStyle: {
+                    backgroundColor: '#7159c1',
+                },
+                headerTintColor: '#fff',
+            },
+        }
+    )
+);
+
+export default Routes;
